7f60e38496bff892bd90e1b4e195dfefc367640f
ben.gould <ben.gould@realmone.com>
2025-04-04 10:56:01 -0400

    renamed and rewired for testing purposes

--- src/main/java/com/realmone/tleasy/TLEasy.java
+++ src/main/java/com/realmone/tleasy/TLEasy.java
@@ -2,6 +2,7 @@ package com.realmone.tleasy;
 
 import com.realmone.tleasy.rest.SimpleTleClient;
 import com.realmone.tleasy.tle.SimpleTleFilter;
+import com.realmone.tleasy.tle.TleUtils;
 
 import javax.net.ssl.SSLException;
 import javax.swing.JButton;
@@ -25,10 +26,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashSet;
 import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class TLEasy extends JFrame {
 
@@ -181,7 +179,7 @@ public class TLEasy extends JFrame {
             protected Long doInBackground() throws Exception {
                 System.out.println("Setting up filter for: " + idField.getText());
                 TleFilter filter = SimpleTleFilter.builder()
-                        .targetNoradIds(getIds())
+                        .targetNoradIds(TleUtils.parseIdentifiers(idField.getText()))
                         .build();
                 Optional<File> saveFile = getSaveFile();
                 if (!saveFile.isPresent()) {
@@ -259,72 +257,6 @@ public class TLEasy extends JFrame {
             return Optional.of(fileChooser.getSelectedFile());
         }
         return Optional.empty();
-    }
-
-    /**
-     * Parses the text from the ID input field to extract valid ID numbers.
-     *
-     * <p>
-     * This method supports two input formats:
-     * <ul>
-     *   <li>Single 5-digit identifiers (e.g., "12345").</li>
-     *   <li>Ranges of 5-digit identifiers specified with a hyphen (e.g., "12345-12350").
-     *       In this case, all numbers in the inclusive range are added to the result.</li>
-     * </ul>
-     * The input string may contain multiple IDs or ranges separated by commas. Whitespace around
-     * commas or hyphens is ignored. If a range is specified where the start number is greater than
-     * the end number, an {@code IllegalArgumentException} is thrown.
-     * </p>
-     *
-     * @return a {@link Set} of ID Strings extracted from the input; returns an empty set if no valid IDs
-     *         are found.
-     */
-    private Set<String> getIds() {
-        String input = idField.getText().trim();
-        Set<String> result = new HashSet<>();
-        // Split input by commas to allow mixed single IDs and ranges
-        String[] parts = input.split("\\s*,\\s*");
-        for (String part : parts) {
-            // If the part is a 5 digit identifier
-            if (part.matches("\\d{5}")) {
-                result.add(part);
-            }
-            // Else if the part is a range specification
-            else if (part.matches("\\d{5}\\s*-\\s*\\d{5}")) {
-                // Grab the start and end part
-                String[] rangeParts = part.split("\\s*-\\s*");
-                int start = Integer.parseInt(rangeParts[0]);
-                int end = Integer.parseInt(rangeParts[1]);
-                // Simple validation to ensure the first number is smaller than the second...
-                if (start > end) {
-                    throw new IllegalArgumentException("Range must be in ascending order.");
-                }
-                // Add all the target numbers in the specified range
-                result.addAll(getNumbersBetween(start, end).stream()
-                        .map(Object::toString)
-                        .collect(Collectors.toSet()));
-            }
-        }
-        return result;
-    }
-
-    /**
-     * Creates a Set of integers representing the range of numbers between the provided start and end values.
-     *
-     * @param start The integer to start the range with
-     * @param end   The integer to end the range with
-     * @return A {@link Set} of {@link Integer}s representing all numbers from the start to the end inclusive
-     */
-    private Set<Integer> getNumbersBetween(int start, int end) {
-        if (start > end) {
-            throw new IllegalArgumentException("Start number must be less than or equal to end number.");
-        }
-
-        Set<Integer> numbers = new HashSet<>();
-        for (int i = start; i <= end; i++) {
-            numbers.add(i);
-        }
-        return numbers;
     }
 
     /**
